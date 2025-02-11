00c24617e3c2114f7d5d5927986467a1067d7af2
Stephen <bolajistephen72@gmail.com>
2025-02-11 10:29:07 +0100

    add global context provider and auth logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
 import { SplashScreen, Stack } from "expo-router";
 import "./globals.css";
-import {useFonts} from "expo-font"
+import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import GlobalProvider from "@/lib/global-provider";
 export default function RootLayout() {
-
   const [fontsLoaded] = useFonts({
     "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
     "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
@@ -11,15 +11,19 @@ export default function RootLayout() {
     "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
     "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
     "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
-  })
+  });
 
   useEffect(() => {
-    if (fontsLoaded){
+    if (fontsLoaded) {
       SplashScreen.hideAsync();
     }
-  },[fontsLoaded])
-  
-  if(!fontsLoaded) return null
+  }, [fontsLoaded]);
 
-  return <Stack screenOptions={{headerShown: false}}/>;
+  if (!fontsLoaded) return null;
+
+  return (
+    <GlobalProvider>
+      <Stack screenOptions={{ headerShown: false }} />;
+    </GlobalProvider>
+  );
 }
